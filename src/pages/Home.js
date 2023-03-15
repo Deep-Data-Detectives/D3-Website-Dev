49e30e6d9753e61ff290a9022467c289e4089b03
JJerrychan <164768186@qq.com>
2023-03-15 11:39:52 -0400

    ourstory page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                  fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
                                  strokeLinejoin="round" className="feather feather-zap">
-                                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+                                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
                             </svg>
                             &nbsp;PLAY THE GAME
                         </Link>
